fa3fde2ea2a4f42858e3bc6ad78e4cff5e9bac1a
ChillyWilly <noreply>
2021-03-02 02:15:05 -0500

    flow logic and user prompts

--- index.js
+++ index.js
@@ -1,159 +1,188 @@
 const pageTemplate = require('./src/page-template');
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
-const inquirer = require('inquirer');
+// const Employee = require('./lib/Employee');
+// const Engineer = require('./lib/Engineer');
+// const Intern = require('./lib/Intern');
+// const Manager = require('./lib/Manager');
+// const inquirer = require('inquirer');
 const fs = require('fs');
-let pageInfo;
+// let pageInfo = [];
+//     let ManagerPrompt = () => {
+//         return inquirer.prompt([{
+//                 type: 'input',
+//                 name: 'name',
+//                 message: "Manager's name:",
+//                 validate: nameInput => {
+//                     if (nameInput) {
+//                         return true
+//                     } else {
+//                         console.log("Enter Manager's name");
+//                         return false;
+//                     }
+//                 }
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'id',
+//                 message: "Manager's id:",
+//                 validate: idInput => {
+//                     if (idInput) {
+//                         return true
+//                     } else {
+//                         console.log("Enter Manager's id");
+//                         return false;
+//                     }
+//                 }
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'email',
+//                 message: "Manager's email:",
+//                 validate: emailInput => {
+//                     if (emailInput) {
+//                         return true
+//                     } else {
+//                         console.log("Enter Manager's email");
+//                         return false;
+//                     }
+//                 }
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'officeNum',
+//                 message: "Manager's office number:",
+//                 validate: officeNumInput => {
+//                     if (officeNumInput) {
+//                         return true
+//                     } else {
+//                         console.log("Enter Manager's office number:");
+//                         return false;
+//                     }
+//                 }
+//             }
+//         ]).then(data => {
+//             const manager = new Manager(data.name, data.id, data.email, data.officeNum);
+//             manager.role = manager.getRole()
+//             AddEmployee(manager);
+//             EmployeePrompt();
+//         })
+//     }
 
-class Prompts {
-    pageInfo = [];
-    AddManager() {
-        return inquirer.prompt([{
-                type: 'input',
-                name: 'name',
-                message: "Manager's name:",
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true
-                    } else {
-                        console.log("Enter Manager's name");
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "Manager's id:",
-                validate: idInput => {
-                    if (idInput) {
-                        return true
-                    } else {
-                        console.log("Enter Manager's id");
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "Manager's email:",
-                validate: emailInput => {
-                    if (emailInput) {
-                        return true
-                    } else {
-                        console.log("Enter Manager's email");
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'officeNum',
-                message: "Manager's office number:",
-                validate: officeNumInput => {
-                    if (officeNumInput) {
-                        return true
-                    } else {
-                        console.log("Enter Manager's office number:");
-                        return false;
-                    }
-                }
-            }
-        ])
+//     let EngineerPrompt = () => {
+//         return inquirer.prompt([{
+//                 type: 'input',
+//                 name: 'name',
+//                 message: "Engineer's name:"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'id',
+//                 message: "Engineer's id:"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'email',
+//                 message: "Engineer's email:"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'github',
+//                 message: "Engineer's github username:"
+//             }
+//         ]).then(data => {
+//             const engineer = new Engineer(data.name, data.id, data.email, data.github);
+//             engineer.role = engineer.getRole();
+//             AddEmployee(engineer);
+//             EmployeePrompt();
+//         })
+//     }
+
+//     let InternPrompt = () => {
+//         return inquirer.prompt([{
+//                 type: 'input',
+//                 name: 'name',
+//                 message: "Intern's name:"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'id',
+//                 message: "Intern's id:"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'email',
+//                 message: "Intern's email:"
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'school',
+//                 message: "Intern's school:"
+//             }
+//         ]).then(data => {
+//             const intern = new Intern(data.name, data.id, data.email, data.school);
+//             intern.role = intern.getRole();
+//             AddEmployee(intern);
+//             EmployeePrompt();
+//         })
+//     }
+
+//     let EmployeePrompt = () => {
+//         return inquirer.prompt([{
+//             type: 'list',
+//             name: 'engineerOrIntern',
+//             message: "Would you like to add more team members or quit?",
+//             choices: ['Engineer', 'Intern', 'Quit']
+//         }, ]).then(data => {
+//             switch (data.engineerOrIntern) {
+//                 case "Engineer":
+//                     console.log("here");
+//                     EngineerPrompt();
+//                     break;
+//                 case "Intern":
+//                     InternPrompt();
+//                     break;
+//                 default:
+//                     console.log(pageInfo);
+//                     const newPage = pageTemplate(pageInfo);
+//                     fs.writeFile('./dist/index.html', newPage, (err) => {
+//                         if (err) throw err;
+//                     })
+//             }
+//         })
+           
+//     }
+
+//     let AddEmployee = (data) => {
+//        return pageInfo.push(data);
+//     }
+
+
+// ManagerPrompt()
+
+let newPage = [
+    {
+      name: 'Mike',
+      id: '1',
+      email: 'guy@place.com',
+      officeNumber: '2',
+      role: 'Manager'
+    },
+    {
+      name: 'Steve',
+      id: '3',
+      email: 'dude@place.com',
+      github: 'PaulKup',
+      role: 'Engineer'
+    },
+    {
+      name: 'Phil',
+      id: '7',
+      email: 'kupiszewskist1@gmail.com',
+      school: 'University of American Samoa',
+      role: 'Intern'
     }
+  ]
+  newPage = pageTemplate(newPage);
 
-    AddEngineer() {
-        return inquirer.prompt([{
-                type: 'input',
-                name: 'name',
-                message: "Engineer's name:"
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "Engineer's id:"
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "Engineer's email:"
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: "Engineer's github username:"
-            }
-        ])
-    }
-
-    addIntern() {
-        return inquirer.prompt([{
-                type: 'input',
-                name: 'name',
-                message: "Intern's name:"
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "Intern's id:"
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "Intern's email:"
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: "Intern's school:"
-            }
-        ])
-    }
-
-    AddEmployee() {
-        addResponse = inquirer.prompt([{
-            type: 'list',
-            name: 'engineerOrIntern',
-            message: "Would you like to add more team members or quit?",
-            choices: ['Engineer', 'Intern', 'Quit']
-        }, ])
-        
-    }
-}
-
-
-const prompts = new Prompts();
-
-prompts.AddManager().then(data => {
-    pageInfo = data;
-    prompts.AddEmployee()
-        .then(AddResponse => {
-            console.log(AddResponse);
-            //     if (info == 'Engineer') {
-            //         engineerPrompt().then(engineerInfo => {
-            //             data.push(engineerInfo);
-            //         })
-            //         } else if (info == 'Intern') {
-            //             InternPrompt().then(InternInfo => {
-            //                 data.push(InternInfo);
-            //             })
-            //         } else if (info == 'Quit') {
-            //             return data;
-            //         } else {
-            //             AddEmployee();
-            //         }
-            // })
-        });
-});
-
-
-// fs.writeFile('./dist/index.html', data, (err) => {
-//     if (err) throw err;
-// })
-
-// const mike = new Employee('mike', '2', 'laskdjf');
-
-// console.log(typeof (mike.getEmail()));
+fs.writeFile('./dist/index.html', newPage, (err) => {
+    if (err) throw err;
+})
--- src/page-template.js
+++ src/page-template.js
@@ -1,27 +1,53 @@
+const EmployeeCardConstructor = employeeInfo => {
+  let extraInfo;
+  return employeeInfo
+    .map(employee => {
+      if (employee.role == 'Manager') {
+        xtraInfo = 'Office number: ' + employee.officeNumber;
+      } else if (employee.role == 'Engineer') {
+        xtraInfo = 'GitHub: ' + employee.github;
+      } else {
+        xtraInfo = 'School: ' + employee.school;
+      }
+      return `<div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title bg-primary">${employee.name}</h5>
+            <h6 class="card-subtitle mb-2 bg-primary">${employee.role}</h6>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${employee.id}</li>
+                <li class="list-group-item">Email: ${employee.email}</li>
+                <li class="list-group-item">${xtraInfo}</li>
+            </ul>
+          </div>
+        </div>`;
+    })
+    .join('')}
+
+
 
 const pageTemplate = employeeInfo => {
-    let xtraInfo;
-    return employeeInfo
-      .map(employee => {
-        if (employee.role == 'manager') {
-          xtraInfo = 'Office number: ' + employee.officeNumber;
-        } else if (employee.role == 'engineer') {
-          xtraInfo = 'GitHub: ' + employee.github;
-        } else {
-          xtraInfo = 'School: ' + employee.school;
-        }
-        return `<div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title bg-primary">${employee.name}</h5>
-              <h6 class="card-subtitle mb-2 bg-primary">${employee.role}</h6>
-              <ul class="list-group list-group-flush">
-                  <li class="list-group-item">ID: ${employee.id}</li>
-                  <li class="list-group-item">Email: ${employee.email}</li>
-                  <li class="list-group-item">${xtraInfo}</li>
-              </ul>
-            </div>
-          </div>`;
-      })
-      .join('');
+    return `
+    <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <header>
+    <h1 class="bg-primary text-white d-flex justify-content-center p-3">My Team</h1>l
+  </header>
+  <main>
+  ${ EmployeeCardConstructor(employeeInfo) }
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  </body>
+    `;
   };
   module.exports = pageTemplate;
